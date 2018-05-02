c60b72ff0a573672a1eb4921f5207b6329fb0454
laviniaeleanor <laviniaeleanor@outlook.com>
2018-05-02 12:35:27 +0200

    Actions changed to test with Questions backend

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,25 +1,25 @@
 import {quizQuestions} from '../dummydata/questions'
-// import * as request from 'superagent'
-//
-// const baseUrl = 'http://localhost:4001'
+import * as request from 'superagent'
+
+const baseUrl = 'http://localhost:4001'
 
 export const FETCH_QUESTIONS = 'FETCH_QUESTIONS'
 export const UPDATE_SCORE = 'UPDATE_SCORE'
 
-// export const fetchQuestions = (quizId) => (dispatch) => {
-//     request
-//       .get(`${baseUrl}/quiz/${quizId}`)
-//       .then(response => dispatch({
-//         type: FETCH_QUESTIONS,
-//         payload: response.body
-//       }))
-//       .catch(err => alert(err))
-// }
+export const fetchQuestions = (quizId) => (dispatch) => {
+    request
+      .get(`${baseUrl}/questions/${quizId}`)
+      .then(response => dispatch({
+        type: FETCH_QUESTIONS,
+        payload: response.body.data
+      }))
+      .catch(err => alert(err))
+}
 
-export const fetchQuestions = () => ({
-  type: FETCH_QUESTIONS,
-  payload: quizQuestions.data
-})
+// export const fetchQuestions = () => ({
+//   type: FETCH_QUESTIONS,
+//   payload: quizQuestions.data
+// })
 
 export const updateScore = () => ({
   type: UPDATE_SCORE
