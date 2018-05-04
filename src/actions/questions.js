463b52190e80d308d4adc177fdd4ed976a39334b
annavanbohemen <anna@vanbohemen.com>
2018-05-04 11:29:40 +0200

    changed actions questions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,28 +1,45 @@
 import {quizQuestions} from '../dummydata/questions'
 import * as request from 'superagent'
 
-const baseUrl = 'http://localhost:4001'
+const baseUrlQuestions = 'http://localhost:4001'
+const baseUrlResponses = 'http://localhost:4002'
 
-export const FETCH_QUESTIONS = 'FETCH_QUESTIONS'
+export const GET_QUESTIONS = 'GET_QUESTIONS'
 export const UPDATE_SCORE = 'UPDATE_SCORE'
 export const SEND_RESPONSE = 'SEND_RESPONSE'
+export const ADD_ANSWER = 'ADD_ANSWER'
+export const UPDATE_ANSWER ='UPDATE_ANSWER'
 
-export const fetchQuestions = (quizId) => (dispatch) => {
+export const getQuestions = (quizId) => (dispatch) => {
     request
-      .get(`${baseUrl}/quizquestions/${quizId}`)
+      .get(`${baseUrlQuestions}/quizquestions/${quizId}`)
       .then(response => dispatch({
-        type: FETCH_QUESTIONS,
+        type: GET_QUESTIONS,
         payload: response.body
       }))
       .catch(err => alert(err))
 }
 
-export const updateScore = () => ({
-  type: UPDATE_SCORE
+
+export const sendResponse = (response) => (dispatch) => {
+  console.log("inside action sendresponse")
+  console.log(response)
+    request
+      .post(`${baseUrlResponses}/responses`)
+      .send(response)
+      .then(response => dispatch({
+        type: SEND_RESPONSE,
+        payload: response
+     }))
+     .catch(err => alert(err))
+}
+
+export const addAnswer = (answer) => ({
+  type: ADD_ANSWER,
+  payload: answer
 })
 
-
-export const sendResponse = (response) => ({
-    type: SEND_RESPONSE,
-    payload: response
+export const updateAnswer = (answer) => ({
+  type: UPDATE_ANSWER,
+  payload: answer
 })
